6ec1ccbae381227225be022dfdba8e74ec94ef99
Julia Bechaire <juliabechaire06@gmail.com>
2026-05-20 18:06:10 -0300

    limpeza

--- src/model/Avaliavel.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public interface Avaliavel {
-    void avaliar(int nota, String resenha);
-    int getNota();
-    String getResenha();
-}
--- src/model/Validavel.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-import exception.DadosInvalidosException;
-
-public interface Validavel {
-    void validar() throws DadosInvalidosException;
-}
