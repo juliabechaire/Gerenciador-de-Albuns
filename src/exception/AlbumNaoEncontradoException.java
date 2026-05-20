e1db443c57eb990eb625398d18d03c2b1234c9a0
Julia Bechaire <juliabechaire06@gmail.com>
2026-05-20 18:08:25 -0300

    Limpeza 0.2

--- src/exception/AlbumNaoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package exception;
-
-public class AlbumNaoEncontradoException extends Exception {
-    private static final long serialVersionUID = 1L;
-    public AlbumNaoEncontradoException(String termoBusca) {
-        super("Nenhum álbum correspondente a '" + termoBusca + "' foi encontrado na biblioteca.");
-    }
-}
--- src/exception/DadosInvalidosException.java
+++
@@ -1,8 +0,0 @@
-package exception;
-
-public class DadosInvalidosException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-    public DadosInvalidosException(String mensagem) {
-        super(mensagem);
-    }
-}
